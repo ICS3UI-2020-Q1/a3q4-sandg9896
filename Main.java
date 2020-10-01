373bcfd517b0ee47e9fe04ccd9b1c92490292a78
sandg9896 <sandg9896@wrdsb.ca>
2020-10-01 15:46:47 +0000

    Positive Integer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Gavin Sandhar
  */
 public class Main {
 
@@ -9,7 +10,48 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user to enter a positive number
+    System.out.println("Please eneter a positive integer");
+     int choice = input.nextInt();
+     
+     // determine number chosen
+    switch(choice){
+      case 1:
+       System.out.println("One");
+       break;
+      case 2:
+       System.out.println("Two");
+       break;
+      case 3:
+       System.out.println("Three");
+       break;
+      case 4:
+       System.out.println("Four");
+       break;
+      case 5:
+       System.out.println("Five");
+       break; 
+      case 6:
+       System.out.println("Six");
+       break;
+      case 7:
+       System.out.println("Seven");
+       break;
+      case 8:
+       System.out.println("Eight");
+       break;
+      case 9:
+       System.out.println("Nine");
+       break;
+      case 10:
+       System.out.println("Ten");
+       break;
+      default:
+       System.out.println("Not in the range"); 
+    }
     
   }
 }
